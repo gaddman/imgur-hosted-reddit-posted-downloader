ff962e82f93b4789d203a5221aa157e0656d1189
Chris Gadd <gaddman@email.com>
2014-05-06 20:59:20 +1200

    Adding capability to call as a function

--- redditImageDownloader.py
+++ redditImageDownloader.py
@@ -13,9 +13,11 @@ from bs4 import BeautifulSoup
 
 
 def download_image(image_url, local_filename):
+    """
+    Download a given URL to a file
+    """
     logging.debug('Download...')
     # explicitly creating session rather than requests.get so session can be closed on completion and avoid warnings of unclosed sockets (in python3)
-    local_filename = os.path.join(save_location, local_filename)
     with requests.Session() as s:
         response = s.get(image_url, stream=True)
         # determine file size in MB (where provided)
@@ -27,13 +29,16 @@ def download_image(image_url, local_filename):
                     fo.write(chunk)
 
 
-def imgur_handler(submission_url):
+def imgur_handler(submission, subreddit, download_location):
+    """
+    Download submissions in the Imgur domain
+    """
     # handle albums and images on imgur.com. Using BeautifulSoup rather than the Imgur API for now
-    if '//imgur.com/a/' in submission_url:
+    if '//imgur.com/a/' in submission.url:
         # album
         logging.debug('Imgur page album')
         with requests.Session() as s:
-            response = s.get(submission_url)
+            response = s.get(submission.url)
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text)
                 matches = soup.select('.album-view-image-link a')
@@ -45,15 +50,15 @@ def imgur_handler(submission_url):
                         image_url = 'http:' + image_url
                     image_filename = image_url.split('/')[-1].split('#')[0].split('?')[0]
                     # add index to the name to retain order
-                    local_filename = 'reddit_{}_{}_{:02d}_{}'.format(args.subreddit, submission.id, index, image_filename)
-                    download_image(image_url, local_filename)
+                    local_filename = 'reddit_{}_{}_{:02d}_{}'.format(subreddit, submission.id, index, image_filename)
+                    download_image(image_url, os.path.join(download_location, local_filename))
 
-    elif '//imgur.com/' in submission_url:
+    elif '//imgur.com/' in submission.url:
         # single image page or a redirect to a single image
         logging.debug('Imgur page')
         # check for a redirection, using head rather than get
         with requests.Session() as s:
-            response = s.head(submission_url, allow_redirects=False)
+            response = s.head(submission.url, allow_redirects=False)
             if response.status_code == 301:
                 # redirected, grab where to (assuming imgur will only redirect once, with status 301)
                 logging.debug('--Imgur page redirect')
@@ -61,7 +66,7 @@ def imgur_handler(submission_url):
             elif response.status_code == 200:
                 # no redirect, go ahead and download the full page
                 logging.debug('--Imgur page single')
-                response = s.get(submission_url)
+                response = s.get(submission.url)
                 soup = BeautifulSoup(response.text)
                 image_url = soup.find('link', rel='image_src')['href']
                 if image_url.startswith('//'):
@@ -69,92 +74,108 @@ def imgur_handler(submission_url):
                     image_url = 'http:' + image_url
 
         image_filename = image_url.split('/')[-1].split('#')[0].split('?')[0]
-        local_filename = 'reddit_{}_{}_{}'.format(args.subreddit, submission.id, image_filename)
-        download_image(image_url, local_filename)
+        local_filename = 'reddit_{}_{}_{}'.format(subreddit, submission.id, image_filename)
+        download_image(image_url, os.path.join(download_location, local_filename))
 
-    elif '//i.imgur.com/' in submission_url:
+    elif '//i.imgur.com/' in submission.url:
         # single image
         logging.debug('Imgur image')
-        image_url = submission_url
+        image_url = submission.url
         image_filename = image_url.split('/')[-1].split('#')[0].split('?')[0]
-        local_filename = 'reddit_{}_{}_{}'.format(args.subreddit, submission.id, image_filename)
-        download_image(image_url, local_filename)
+        local_filename = 'reddit_{}_{}_{}'.format(subreddit, submission.id, image_filename)
+        download_image(image_url, os.path.join(download_location, local_filename))
 
 
-# define and parse arguments
-parser = argparse.ArgumentParser()
-parser = argparse.ArgumentParser(description="Download images from specified subreddit")
-parser.add_argument("subreddit", help="subreddit to download from", type=str)
-parser.add_argument("-d", "--save_location", help="location to store downloaded images", default=".", type=str)
-parser.add_argument("-s", "--score", help="minimum score required to download", default=500, type=int)
-parser.add_argument("-p", "--period", help="period of interest (day, week, month)", default="day", type=str)
-parser.add_argument("-l", "--logfile", help="filename for logging", type=str)
-parser.add_argument("-m", "--max", help="maximum number of submissions", default=25, type=int)
-parser.add_argument("-q", "--quiet", help="suppress output", action='store_true', default=False)
-args = parser.parse_args()
-save_location = os.path.abspath(args.save_location)
+def getargs():
+    """
+    Just grab all the command line arguments and return
+    """
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Download images from specified subreddit")
+    parser.add_argument("subreddit", help="subreddit to download from", type=str)
+    parser.add_argument("-p", "--period", help="period of interest (day, week, month)", default="day", type=str)
+    parser.add_argument("-s", "--score", help="minimum score required to download", default=500, type=int)
+    parser.add_argument("-d", "--download_location", help="location to store downloaded images", default=".", type=str)
+    parser.add_argument("-l", "--logfile", help="filename for logging", type=str)
+    parser.add_argument("-m", "--max", help="maximum number of submissions", default=25, type=int)
+    parser.add_argument("-q", "--quiet", help="suppress output", action='store_true', default=False)
+    args = parser.parse_args()
+    args.download_location = os.path.abspath(args.download_location)
 
-# setup logging to file and stderr
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-# suppress logs from the requests module hitting root logger
-logging.getLogger('urllib3').propagate = False
+    return args
 
-if args.logfile:
-    fh = logging.FileHandler(filename=args.logfile)
-    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s %(message)s')
-    fh.setFormatter(formatter)
-    fh.setLevel(logging.DEBUG)
-    logger.addHandler(fh)
-    logging.getLogger('urllib3').addHandler(fh)
 
-if not args.quiet:
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    logger.addHandler(ch)
+def reddit_image_downloader(subreddit, period='day', score=500, max=25, logfile=None, download_location='.', quiet=True):
+    """
+    Download images from a chosen Reddit subreddit
+    """
 
-logging.info('Beginning scrape of /r/{} subreddit (top this {}, score>{}) to {}'.format(args.subreddit, args.period, args.score, save_location))
+    # setup logging to file and stderr
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    # suppress logs from the requests module hitting root logger
+    logging.getLogger('urllib3').propagate = False
 
-# Connect to reddit and download the subreddit front page
-r = praw.Reddit(user_agent='redditImageDownloader/1.1 (https://github.com/gaddman/redditImageDownloader)')
-if args.period == 'day':
-    submissions = r.get_subreddit(args.subreddit).get_top_from_day(limit=args.max)
-elif args.period == 'week':
-    submissions = r.get_subreddit(args.subreddit).get_top_from_week(limit=args.max)
-elif args.period == 'month':
-    submissions = r.get_subreddit(args.subreddit).get_top_from_month(limit=args.max)
+    if logfile:
+        fh = logging.FileHandler(filename=logfile)
+        formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s %(message)s')
+        fh.setFormatter(formatter)
+        fh.setLevel(logging.DEBUG)
+        logger.addHandler(fh)
+        logging.getLogger('urllib3').addHandler(fh)
 
-# Process all the submissions
-try:
-    for submission in submissions:
-        # Check for all the cases where we will skip a submission:
-        if submission.score < args.score:
-            logging.info('Score too low ({}): "{}" at {}'.format(submission.score, submission.title, submission.url))
-            continue  # skip submissions that haven't even reached required score
-        if len(glob.glob(os.path.join(save_location, 'reddit_{}_{}_*'.format(args.subreddit, submission.id)))) > 0:
-            logging.info('Already downloaded: "{}" at {}'.format(submission.title, submission.url))
-            continue  # we've already downloaded files for this reddit submission
+    if not quiet:
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        logger.addHandler(ch)
 
-        logging.info('Good submission (score {}): "{}" at {}'.format(submission.score, submission.title, submission.url))
+    logging.info('Beginning scrape of /r/{} subreddit (top this {}, score>{}) to {}'.format(
+        subreddit, period, score, download_location))
 
-        if 'imgur.com/' in submission.url:
-            # This is an Imgur submission, we can deal with this
-            imgur_handler(submission.url)
-        else:
-            # non-Imgur URL, let's see what can be done.
-            # Only interested in images, ignore redirects or links to HTML pages
-            logging.debug('Non-imgur')
-            with requests.Session() as s:
-                response = s.head(submission.url)
-                if 'image/' in response.headers['Content-Type']:
-                    image_url = submission.url
-                    image_filename = image_url.split('/')[-1].split('#')[0].split('?')[0]
-                    local_filename = 'reddit_{}_{}_{}'.format(args.subreddit, submission.id, image_filename)
-                    download_image(image_url, local_filename)
-                else:
-                    logging.warning("'Content-Type' not suitable ({})".format(response.headers['Content-Type']))
+    # Connect to reddit and download the subreddit front page
+    r = praw.Reddit(user_agent='redditImageDownloader/1.2 (https://github.com/gaddman/redditImageDownloader)')
+    if period == 'day':
+        submissions = r.get_subreddit(subreddit).get_top_from_day(limit=max)
+    elif period == 'week':
+        submissions = r.get_subreddit(subreddit).get_top_from_week(limit=max)
+    elif period == 'month':
+        submissions = r.get_subreddit(subreddit).get_top_from_month(limit=max)
 
-    logging.info('Completed scrape')
+    # Process all the submissions
+    try:
+        for submission in submissions:
+            # Check for all the cases where we will skip a submission:
+            if submission.score < score:
+                logging.info('Score too low ({}): "{}" at {}'.format(submission.score, submission.title, submission.url))
+                continue  # skip submissions that haven't even reached required score
+            if len(glob.glob(os.path.join(download_location, 'reddit_{}_{}_*'.format(subreddit, submission.id)))) > 0:
+                logging.info('Already downloaded: "{}" at {}'.format(submission.title, submission.url))
+                continue  # we've already downloaded files for this reddit submission
 
-except (praw.errors.InvalidSubreddit, praw.errors.RedirectException):
-    logging.error("Invalid subreddit: {}".format(args.subreddit))
+            logging.info('Good submission (score {}): "{}" at {}'.format(submission.score, submission.title, submission.url))
+
+            if 'imgur.com/' in submission.url:
+                # This is an Imgur submission, we can deal with this
+                imgur_handler(submission, subreddit, download_location)
+            else:
+                # non-Imgur URL, let's see what can be done.
+                # Only interested in images, ignore redirects or links to HTML pages
+                logging.debug('Non-imgur')
+                with requests.Session() as s:
+                    response = s.head(submission.url)
+                    if 'image/' in response.headers['Content-Type']:
+                        image_url = submission.url
+                        image_filename = image_url.split('/')[-1].split('#')[0].split('?')[0]
+                        local_filename = 'reddit_{}_{}_{}'.format(subreddit, submission.id, image_filename)
+                        download_image(image_url, os.path.join(download_location, local_filename))
+                    else:
+                        logging.warning("'Content-Type' not suitable ({})".format(response.headers['Content-Type']))
+
+        logging.info('Completed scrape')
+
+    except (praw.errors.InvalidSubreddit, praw.errors.RedirectException):
+        logging.error("Invalid subreddit: {}".format(subreddit))
+
+if __name__ == '__main__':
+    args = getargs()
+    reddit_image_downloader(args.subreddit, args.period, args.score, args.max, args.logfile, args.download_location, args.quiet)
